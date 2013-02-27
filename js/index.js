04129136779ad62ad6f17b5660c9eb5f1b094a4d
Lucas de Senna <lucasdesenna@gmail.com>
2013-02-27 19:47:36 -0300

    pagina de pesquisa

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ function setup() {
 		$(this).hide();
 	});
 }
+
+function toggleChildren(target) {
+	$(target).siblings().each(function(index, element) {
+        $(element).children("ul").slideUp(400);
+    });
+	setTimeout(function() {$(target).children().slideDown()}, 400);
+}
